2a11554c83fefab7a47bc5718f26bc272c0c0910
America-first-melon <969372261@qq.com>
2017-05-03 17:40:35 +0800

    add another func for appState

--- appState.js
+++ appState.js
@@ -14,5 +14,19 @@ class AppState {
     // }
 }
 
+// class AppState {
+//   @observable timer = 0;
+
+//   constructor() {
+//     setInterval(() => {
+//       this.timer += 1;
+//     }, 1000);
+//   }
+
+//   resetTimer() {
+//     this.timer = 0;
+//   }
+// }
+
 export default AppState;
 
